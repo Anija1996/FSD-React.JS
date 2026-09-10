a9936f2ed3dfe8e222806c87784db1cba4851390
Anija1996 <anijasarath1996@gmail.com>
2026-09-10 12:01:26 +0530

    API Integration

--- session-31/API Integration/src/App.jsx
+++ session-31/API Integration/src/App.jsx
@@ -148,7 +148,7 @@
 // )
 // }
 
-// Task-4 - etch users and display only selected information
+// Task-4 - fetch users and display only selected information
 //----------------------------------------------------------
 // import { useState,useEffect } from "react";
 
@@ -195,4 +195,298 @@
 //       </div>)
 //     })
 //   )
+// }
+
+// Task-5 - Add a user count
+//--------------------------
+// import { useState,useEffect } from "react";
+
+// export default function App(){
+//     const [user,setUser] = useState([]);
+//     const [loading,setLoading] = useState(true);
+//     const [error,setError] = useState(null);
+
+//     useEffect(()=>{
+//         async function getUserDetails() {
+//             try{
+//                 const response = await fetch ("https://jsonplaceholder.typicode.com/users");
+//                 if(!response.ok){
+//                     throw new Error("Fetching failed");
+//                 }
+//                 const data = await response.json();
+//                 setUser(data);
+//                 // console.log(user);
+                
+//             }
+//             catch(error){
+//                 setError(error.message);
+//             }
+//             finally{
+//                 setLoading(false);
+//             }
+//         }
+
+//         getUserDetails();
+//     },[])
+
+//     if(loading){
+//         return(<p>Loading...</p>);
+//     }
+
+//     if(error){
+//         return(<p>{error}</p>);
+//     }
+
+
+    
+//     return(
+//         <>
+//         <h2>Total User: {user.length}</h2>
+//         {user.map((usr)=>{
+//             return(
+//                 <div key={usr.id}>
+//                     <p>{usr.name}</p>
+//                     <p>{usr.email}</p><br/>
+//                 </div>
+//             )
+//         })}
+//     </>)
+// }
+
+//Task-6 - Fetch a single post
+//---------------------------
+// import { useState,useEffect } from "react";
+
+// export default function App(){
+//     const [post,setPost] = useState(null);
+//     const [loading,setLoading] = useState(true);
+//     const [error,setError] = useState(null);
+
+//     useEffect(()=>{
+//         async function getDetails() {
+//             try{
+//                 const resp = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+//                 if (!resp.ok) {
+//                     throw new Error("Failed to fetch post");
+//                 }
+//                 const data = await resp.json();
+//                 setPost(data);
+//             } catch(error){
+//                 setError(error.message);
+//             } finally{
+//                 setLoading(false);
+//             }
+//         }
+//         getDetails();
+//     },[])
+
+//     if(loading){
+//         return(<p>Loading...</p>);
+//     }
+
+//     if(error){
+//         return(<p>{error}</p>);
+//     }
+
+//     return(
+//         <>
+//             <h2>Title: {post.title}</h2>
+//             <p>Body: {post.body}</p>
+//         </>
+//     )
+// }
+
+//Task-7 - Create a post API
+//--------------------------
+// import { useState } from "react";
+
+// export default function App(){
+//     const [title,setTitle] = useState("");
+//     const [body,setBody] = useState("");
+//     const [loading,setLoading] = useState(false);
+//     const [error,setError] = useState(null);
+
+//         async function postDetails() {
+//             try{
+//                 setLoading(true);
+//                 const resp = await fetch("https://jsonplaceholder.typicode.com/posts",
+//                     {
+//                     method: "POST",
+//                     headers:{
+//                         "Content-Type" : "application/json"
+//                     },
+//                     body: JSON.stringify({
+//                         title,
+//                     body
+//                     })
+//                     }
+                    
+//                 );
+
+//                 if(!resp.ok){
+//                         throw new Error("Posting failed");
+//                 }
+//                 const data = await resp.json();
+//                 console.log(data);
+                
+
+//             } catch(error){
+//                 setError(error.message);
+//             } finally{
+//                 setLoading(false);
+//             }
+//         }
+//         // postDetails();
+
+//     if(loading){
+//             return(<p>Loading...</p>);
+//     }
+
+//     if(error){
+//         return(<p>{error}</p>);
+//     }
+    
+//     return(
+//         <>
+//             <input type="text" onChange={(e)=>setTitle(e.target.value)} />
+//             <input type="text" onChange={(e)=>setBody(e.target.value)} />
+//             <button onClick={postDetails}>Submit</button>
+//         </>
+//     )
+
+// }
+
+//Task-8 - Display the POST response
+//----------------------------------
+// import { useState } from "react"; 
+
+// export default function App(){
+//     const [title,setTitle] = useState("");
+//     const [body,setBody] = useState("");
+//     const [loading,setLoading] = useState(false);
+//     const [error,setError] = useState(null);
+//     const [post,setPost] = useState(null);
+
+//         async function postDetails() {
+//             try{
+                
+//                 setLoading(true);
+//                 const resp = await fetch("https://jsonplaceholder.typicode.com/posts",
+//                     {
+//                     method: "POST",
+//                     headers:{
+//                         "Content-Type" : "application/json"
+//                     },
+//                     body: JSON.stringify({
+//                         title,
+//                     body
+//                     })
+//                     }
+                    
+//                 );
+
+//                 if(!resp.ok){
+//                         throw new Error("Posting failed");
+//                 }
+//                 const data = await resp.json();
+//                 setPost(data);
+//                 setTitle("");
+//                 setBody("");
+                
+
+//             } catch(error){
+//                 setError(error.message);
+//             } finally{
+//                 setLoading(false);
+//             }
+//         }
+//         // postDetails();
+
+//     if(loading){
+//             return(<p>Loading...</p>);
+//     }
+
+//     if(error){
+//         return(<p>{error}</p>);
+//     }
+    
+//     return(
+//         <>
+//             <input type="text" onChange={(e)=>setTitle(e.target.value)} />
+//             <input type="text" onChange={(e)=>setBody(e.target.value)} />
+//             <button onClick={()=>{
+//                 body.length===0 || title.length===0 ? setError("Input box cannot be empty") : postDetails()}
+//             }>Submit</button>
+//             {post && (
+//                 <>
+//                     <h3>{post.title}</h3>
+//                     <p>{post.body}</p>
+//                 </>
+//             )}
+//         </>
+//     )
+
+// }
+
+
+// Task-9 - POST task: Show a success message
+//-------------------------------------------
+// import { useState } from "react";
+
+// export default function App(){
+//     const [post,setPost] = useState("");
+//     const [loading,setLoading] = useState(false);
+//     const [error,setError] = useState(null);
+//     const [title,setTitle] = useState("");
+//     const [body,setBody] = useState("");
+//     const [successMessage, setSuccessMessage] = useState("");
+
+//     async function postDetails() {
+//         try{
+//             setLoading(true);
+//             const resp = await fetch("https://jsonplaceholder.typicode.com/posts",
+//                 {
+//                     method: "POST",
+//                     headers: {
+//                         "Content-Type": "application/json"
+//                     },
+//                     body: JSON.stringify({
+//                         title,
+//                         body
+//                     })
+//                 }
+//             )
+//             if(!resp.ok){
+//                 throw new Error("Failed to fetch");
+//             }
+//             const data = await resp.json();
+//             setPost(data);
+//             setSuccessMessage("Post created successfully!");
+//         } catch(error){
+//             setError(error.message);
+//         } finally{
+//             setLoading(false)
+//         }
+//     }
+
+//     if(loading){
+//         return(<p>Loading...</p>)
+//     }
+
+//     if(error){
+//         return(<p>{error}</p>)
+//     }
+
+//     return(
+//         <>
+//             <input onChange={(e)=>setTitle(e.target.value)}/>
+//             <input onChange={(e)=>setBody(e.target.value)}/>
+//             <button onClick={()=>body.length===0 || title.length===0 ? setError("Input box empty") : postDetails()}>Submit</button>
+//             {successMessage && (
+//                 <p>
+//                     {successMessage}
+//                 </p>
+//             )}
+//         </>
+//     )
 // }
