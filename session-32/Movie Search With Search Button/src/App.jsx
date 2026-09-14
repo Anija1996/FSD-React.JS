cafd08f7ada9529a7fbf599e586a2049611742aa
Anija1996 <anijasarath1996@gmail.com>
2026-09-14 22:44:54 +0530

    Movie Search Using Search Button

---
+++ session-32/Movie Search With Search Button/src/App.jsx
@@ -0,0 +1,85 @@
+import { useState,useEffect} from 'react'
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import MovieCard from './components/MovieCard';
+import Col from 'react-bootstrap/esm/Col';
+import Row from 'react-bootstrap/esm/Row';
+
+
+export default function App() {
+
+  const [loading,setLoading] = useState(true);
+  const [error,setError] = useState(null);
+  const [movies,setMovies] = useState([]);
+  const [filteredMovies,setFilteredMovies] = useState([]);
+  const [search,setSearch] = useState("");
+
+  useEffect(()=>{
+    async function getMovies() {
+      try {
+        const resp = await axios("https://api.tvmaze.com/shows");
+        setMovies(resp.data);
+        setFilteredMovies(resp.data);
+      } catch (error) {
+          setError(error.message);
+      } finally{
+        setLoading(false);
+      }
+    }
+    getMovies();
+  },[])
+
+  function handleSearch()
+  {
+    const result = movies.filter((movie)=>movie.name.toLowerCase().includes(search.toLowerCase()));
+    setFilteredMovies(result);
+  }
+
+  if(loading){
+    return(<p>Loading...</p>)
+  }
+
+  if(error){
+    return(<p>{error}</p>)
+  }
+  return (
+    <>
+      <h2 className='fw-bold'>Movie Search</h2>
+
+      <div className='mx-auto my-2'>
+      <InputGroup className="mb-3">
+        <Form.Control
+          placeholder="Enter the movie name"
+          onChange={(e)=>setSearch(e.target.value)}
+        />
+        <Button 
+          className='btn btn-warning'
+          onClick={handleSearch}
+          >
+          Search
+        </Button>
+      </InputGroup>
+      </div>
+
+      <Row className="mx-5">
+        {filteredMovies.map((movie)=>{
+          return(
+            <Col sm={12} md={6} lg={4} className='mb-5' key={movie.id}>
+              <MovieCard 
+              poster={movie.image?.medium}
+              name={movie.name}
+              genre={movie.genre}
+              year={movie.premiered}
+              rating={movie.average?.rating}
+              />
+            </Col>
+          )
+        })}
+      </Row>
+    </>
+  )
+}
+
